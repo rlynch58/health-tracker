1fd72649e760db40a5bdb26b37a2e25373165513
rlynch58 <robertlynch.tx@gmail.com>
2026-07-27 16:28:25 -0500

    Remove smoke-test code from ContentView

--- HealthTracker/HealthTracker/ContentView.swift
+++ HealthTracker/HealthTracker/ContentView.swift
@@ -6,12 +6,8 @@
 //
 
 import SwiftUI
-import SwiftData
-import HealthTrackerData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,16 +16,6 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
-        .onAppear {
-            let store = DailyLogStore(context: modelContext)
-            let testDateKey = "2026-07-27-SMOKETEST"
-            let log = store.fetchOrCreate(dateKey: testDateKey)
-            log.waterOz = 64
-            try? modelContext.save()
-
-            let readBack = store.fetchOrCreate(dateKey: testDateKey)
-            print("SMOKE TEST: dateKey=\(readBack.dateKey), waterOz=\(String(describing: readBack.waterOz))")
-        }
     }
 }
 
